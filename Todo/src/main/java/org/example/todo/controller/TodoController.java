ee0573bfc3524ef1656b18d1bb181dcaa563e183
Szczapa <gilabertadam@gmail.com>
2024-07-18 16:21:43 +0200

    Update TodoController.java

--- Todo/src/main/java/org/example/todo/controller/TodoController.java
+++ Todo/src/main/java/org/example/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +35,7 @@ public class TodoController {
         return "todo/view";
     }
 
-    @GetMapping("/todo/add")
+    @PostMapping("/todo/add")
     public String todoAdd() {
         Todo newTodo = new Todo(todoService.getNextId(), "New Todo", "New Todo Description", false);
         todoService.add(newTodo);
